5d7f3a2a6d03f74624319a80e300572935a10fc0
Hussein Esmail <hussein.esmail7@gmail.com>
2021-11-11 17:34:04 -0500

    modified file

---
+++ rss_reddit_imgdl.py
@@ -0,0 +1,38 @@
+'''
+rss_reddit_imgdl.py
+Hussein Esmail
+Created: 2021 11 11
+Updated: 2021 11 11
+Description: This program looks at Reddit RSS feed files, and downloads an
+    offline copy of all images an videos (whatever it can) so that the images
+    can be viewed offline. This is meant to reduce the number of times the user
+    needs to open a browser while looking at their RSS feed reader.
+'''
+
+
+import os
+import sys
+
+def main():
+    # CODE HERE
+    """
+    1. Get Reddit feed from URL
+    2. Look for base URLs (ex: "https://i.redd.it/")
+    3. Download image
+    4. Replace entire found URL in new file
+    5. Save entire new file as .xml in a file path
+
+    Notes:
+    - Step 4 is not with step 2 because the program may not be able to get the
+    media.
+    """
+
+    # Get Reddit feed from URL
+
+
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
