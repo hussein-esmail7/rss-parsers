d621c76b78f12d0ebc4770bd53c29d3f36499860
Hussein Esmail <hussein.esmail7@gmail.com>
2021-12-29 15:15:02 -0500

    Iterating images over a given array

--- rss_reddit_imgdl.py
+++ rss_reddit_imgdl.py
@@ -2,13 +2,12 @@
 rss_reddit_imgdl.py
 Hussein Esmail
 Created: 2021 11 11
-Updated: 2021 11 11
+Updated: 2021 12 29
 Description: This program looks at Reddit RSS feed files, and downloads an
     offline copy of all images an videos (whatever it can) so that the images
     can be viewed offline. This is meant to reduce the number of times the user
     needs to open a browser while looking at their RSS feed reader.
 '''
-
 
 import os
 import sys
@@ -41,7 +40,13 @@ str_prefix_err          = f"[{color_red}ERROR{color_end}]\t "
 str_prefix_done         = f"[{color_green}DONE{color_end}]\t "
 str_prefix_info         = f"[{color_cyan}INFO{color_end}]\t "
 
-msg_error_not_reddit = "[ERROR] Not a subreddit RSS URL"
+msg_error_not_reddit    = str_prefix_err + "Not a subreddit RSS URL"
+msg_error_not_url       = str_prefix_err + "Not a URL: "
+q_print_contents        = str_prefix_q + "Print file contents? "
+done_rss_written        = str_prefix_done + "RSS File written"
+str_reddit_image_url    = "https://i.redd.it"
+urls_to_convert         = ["https://reddit.com/r/unixporn/search.rss?q=flair:'Screenshot'&sort=new&restrict_sr=on&feature=legacy_search"]
+
 
 def yes_or_no(str_ask):
     while True:
@@ -77,41 +82,33 @@ def main():
             bool_continue_asking = not yes_or_no("Is this correct: " + file_decoded + "? ")
         file_decoded = open(file_decoded, "r").readlines()
     else:
-        # Get Reddit feed from URL
-        # TODO: This part will use sys.argv later
-        URL_get = "https://reddit.com/r/unixporn/search.rss?q=flair:'Screenshot'&sort=new&restrict_sr=on&feature=legacy_search"
-
-        # Checks if it is a URL
-        if not validators.url(URL_get):
-            print("This is not a URL!")
-            sys.exit(1)
-
-        # Download the RSS file contents
-        file_decoded = urllib.request.urlopen(URL_get).read().decode("utf-8")
-    if yes_or_no("Print file contents? "):
-        print(file_decoded)
-
-    # Look for base URLs (ex: "https://i.redd.it/")
-    #print("".join(file_decoded).find("https://i.redd.it"))
-    link_positions = [m.start() for m in re.finditer("https://i.redd.it", file_decoded)]
-    print("There are " + str(len(link_positions)) + " image URLs")
-    print("Indexes of image URLs (start positons): ", end="")
-    print(link_positions)
-    
-    file_new = file_decoded # file_new is copy of RSS with edited URLs
-    for link in link_positions:
-        img_url = re.match("(.*?)&", file_decoded[link:]).group()[:-1] # Get URL
-        # Download image
-        img_name = img_url.split("/")[-1] # Get image name from URL
-        print("Saving " + img_name + "... ", end="")
-        urllib.request.urlretrieve(img_url, path_img_save + img_name) # Save image
-        # Replace entire found URL in new file
-        file_new = file_new.replace(img_url, "file://" + path_img_save + img_name)
-        print("Done")
-    # Save entire new file as .xml in a file path
-    with open(path_rss_save + "rss1.xml", "w") as f:
-        f.write(file_new)
-    print("RSS File written")
+        print("Test")
+    for URL_get in urls_to_convert:
+        if validators.url(URL_get) or URL_get.startswith("file://"): # Checks if it is a URL or file location
+            # Download the RSS file contents
+            if validators.url(URL_get):
+                file_decoded = urllib.request.urlopen(URL_get).read().decode("utf-8")
+            elif os.path.exists(URL_get.replace("file://", ""):
+                file_decoded = ''.join(open(URL_get, 'r').readlines()).replace("\n", "")
+            # Calculate RSS file name
+            rss_file_name = URL_get[URL_get.index("/r/")+3:]
+            rss_file_name = re.search(r'\W+', rss_file_name).start()
+            # Look for base URLs (ex: "https://i.redd.it/")
+            link_positions = [m.start() for m in re.finditer(str_reddit_image_url, file_decoded)]
+            for link in link_positions:
+                img_url = re.match("(.*?)&", file_decoded[link:]).group()[:-1] # Get URL
+                img_name = img_url.split("/")[-1] # Get image name from URL
+                print("Saving " + img_name + "... ", end="")
+                urllib.request.urlretrieve(img_url, path_img_save + img_name) # Save image
+                # Replace entire found URL in new file
+                file_decoded = file_decoded.replace(img_url, "file://" + path_img_save + img_name)
+                print("Done")
+            # Save entire new file as .xml in a file path
+            with open(path_rss_save + "rss1.xml", "w") as f:
+                f.write(file_decoded)
+            print(done_rss_written)
+        else: # Inputted string is not URL
+            print(msg_error_not_url + URL_get)
     sys.exit()
 
 if __name__ == "__main__":
