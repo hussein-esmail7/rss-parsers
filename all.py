fe050640c1ebbdc77f0e226d1491801cb045fb7f
Hussein Esmail <hussein.esmail7@gmail.com>
2022-01-03 13:43:41 -0500

    modified file

---
+++ all.py
@@ -0,0 +1,65 @@
+'''
+all.py
+Hussein Esmail
+Created: 2022 01 03
+Updated: 2022 01 03
+Description: This program is meant to run all the other RSS parser programs in
+    this folder. The reason this is not a .bashrc alias is because arguments
+    that are parsed to this program will be carried over to all the programs
+    this one runs, such as --verbose, --quiet, --version, etc.
+'''
+
+import os
+import sys
+import glob # Lists all files in directory with wildcard
+
+# ========= VARIABLES ===========
+paths_exclude = ["rss_html.py"] # Programs to exclude even if they meet all requirements
+
+# ========= COLOR CODES =========
+color_end               = '\033[0m'
+color_darkgrey          = '\033[90m'
+color_red               = '\033[91m'
+color_green             = '\033[92m'
+color_yellow            = '\033[93m'
+color_blue              = '\033[94m'
+color_pink              = '\033[95m'
+color_cyan              = '\033[96m'
+color_white             = '\033[97m'
+color_grey              = '\033[98m'
+
+# ========= COLORED STRINGS =========
+str_prefix_q            = f"[{color_pink}Q{color_end}]"
+str_prefix_y_n          = f"[{color_pink}y/n{color_end}]"
+str_prefix_ques         = f"{str_prefix_q}\t "
+str_prefix_err          = f"[{color_red}ERROR{color_end}]\t "
+str_prefix_done         = f"[{color_green}DONE{color_end}]\t "
+
+def yes_or_no(str_ask):
+    while True:
+        y_n = input(f"{str_prefix_q} {str_prefix_y_n} {str_ask}").lower()
+        if y_n[0] == "y":
+            return True
+        elif y_n[0] == "n":
+            return False
+        if y_n[0] == "q":
+            sys.exit()
+        else:
+            print(f"{str_prefix_err} {error_neither_y_n}")
+
+def main():
+    # Path of where this Python file is
+    path_this_file = os.path.dirname(os.path.realpath(__file__))
+    # Program must contain "rss" in filename and be in same folder
+    programs_run = glob.glob(path_this_file + "/*rss*.py")
+    for program in programs_run:
+        if program.split("/")[-1] in paths_exclude:
+            programs_run.remove(program)
+            print(color_red + "I will not run " + program + color_end)
+        else:
+            print("I will run " + program)
+
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
