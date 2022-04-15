e4efe9bc1ae00d38c43538c6c49f20c04e21f210
Hussein Esmail <hussein.esmail7@gmail.com>
2022-04-14 21:35:21 -0400

    Working

--- rss_vsco.py
+++ rss_vsco.py
@@ -14,11 +14,12 @@ import urllib.request
 import getpass # Used to get username
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service # Used to set Chrome location
-from selenium.webdriver.chrome.options import Options # Used to add aditional settings (ex. run in background)
-from selenium.webdriver.common.by import By # Used to determine type to search for (normally By.XPATH)
+from selenium.webdriver.chrome.service import Service # To set Chrome location
+from selenium.webdriver.chrome.options import Options # For "run in background"
+from selenium.webdriver.common.by import By # Used to get type to search for
 
 # ========= VARIABLES ===========
+BOOL_PRINTS             = True
 RSS_FOLDER              = os.path.expanduser("~/Documents/Local-RSS/VSCO/")
 RSS_TERM                = "VSCO"
 VSCO_URLS               = RSS_FOLDER + "urls" # Folder location for URLs list
@@ -63,44 +64,58 @@ def main():
     if not os.path.exists(VSCO_URLS):
         print(f"{str_prefix_err}: {VSCO_URLS} does not exist. Please create the file in this folder and run the program again.")
         sys.exit(1)
+    elif BOOL_PRINTS:
+        print(f"{str_prefix_info}urls file exists - {VSCO_URLS}")
     url_file_lines = open(VSCO_URLS, 'r').readlines()
     usernames = []
-    for line in usernames:
+    for line in url_file_lines:
         # This is for processing URLs and removing comments and whitespace
         line_first_word = line.strip().split()[0] # Get first word (since URLs don't have spaces)
         if line_first_word[0] != "#":
             # If it is not a comment
             # Treat it as a link. The link checker will verify if it is valid
             usernames.append(line_first_word)
-    options = Options()  
+            print(line_first_word)
+    options = Options()
     options.add_argument("--headless")  # Run in background
     service = Service(ChromeDriverManager(log_level=0).install())
     driver = webdriver.Chrome(service=service, options=options)
+    if BOOL_PRINTS:
+        print(f"{str_prefix_info}Usenames: {len(usernames)}")
+
     for username in usernames:
         site_base = f"https://vsco.co/{username}/"
         target_site = f"{site_base}gallery"
         urls_images = []
         dict_urls = []
         lines_new = []
-        RSS_DESCRIPTION = f"VSCO pictures by @{username}" # Used for RSS feed posts
+        RSS_DESCRIPTION = f"VSCO pictures by @{username}" # For RSS feed posts
         RSS_TITLE = f"VSCO - @{username}"
-        
+        if BOOL_PRINTS:
+            print(f"{str_prefix_info}User: {username}")
+
         if not os.path.exists(RSS_FOLDER): # Make dir if it does not exist
             os.makedirs(RSS_FOLDER)
-        if os.path.exists(RSS_FOLDER + f"{username}.xml"): # If RSS fole exists, read it only
+        if os.path.exists(RSS_FOLDER + f"{username}.xml"):
+            # If file exists, read only
             lines = open(RSS_FOLDER + f"{username}.xml", 'r').readlines()
-        else:                                                           # Make RSS if it does not exist
+        else:
+            # Make RSS if it does not exist
             urllib.request.urlretrieve(URL_TEMPLATE_RSS, RSS_FOLDER + f"{username}.xml")
             lines = open(RSS_FOLDER + f"{username}.xml", 'r').readlines()
-            for num, _ in enumerate(lines):                             # Replace info in template
+            for num, _ in enumerate(lines):
+                # Replace info in template
                 lines[num] = lines[num].replace("[RSS FEED TITLE]", RSS_TITLE).replace("[TERM]", RSS_TERM).replace("[RSS DESCRIPTION]", RSS_DESCRIPTION)
                 for rep in RSS_LINES_REMOVE:
                     lines[num] = lines[num].replace('    ' + rep + '\n', "")
-            open(RSS_FOLDER + f"{username}.xml", 'w').writelines(lines)      # Replace template lines with filled in info
-        
+            open(RSS_FOLDER + f"{username}.xml", 'w').writelines(lines)
+            # Replace template lines with filled in info
+
         driver.set_window_size(200, 1000)
         driver.get(target_site)
         urls_all = [item.get_attribute("href") for item in driver.find_elements(By.XPATH, "//a[@href]")]
+        if BOOL_PRINTS:
+            print(f"{str_prefix_info}\t This page has {len(urls_all)}")
         for item in urls_all:
             if f"{site_base}media/" in item:
                 urls_images.append(item)
@@ -130,7 +145,8 @@ def main():
             for item in description_possible:
                 if "css-1whdjid-Caption" in item.get_attribute("class") and len(item.text) != 0:
                     entry['desc'] = item.text
-            if not is_in_list(entry["url_html"], lines): # If this post is not already in RSS, add it
+            if not is_in_list(entry["url_html"], lines):
+                # If post is not in RSS, add it
                 # These variables are used for the RSS post
                 url_html = entry["url_html"] # URL of the post
                 content_desc = f"<p>{entry['desc']}</p>"
@@ -141,8 +157,10 @@ def main():
                 # Converting HTML text to RSS HTML
                 content_desc = content_desc.replace("\n", "")
                 content_desc = content_desc.replace("\t", "")
-                content_desc = content_desc.replace("&#13;", "")        # :before and :after (not needed)
-                content_desc = content_desc.replace("&", "&amp;")       # Replace HTML elements in RSS description
+                # ":before" and ":after" (not needed)
+                content_desc = content_desc.replace("&#13;", "")
+                # Replace HTML elements in RSS description
+                content_desc = content_desc.replace("&", "&amp;")
                 content_desc = content_desc.replace("'", "&apos;")
                 content_desc = content_desc.replace('"', "&quot;")
                 content_desc = content_desc.replace("<", "&lt;")
@@ -155,9 +173,11 @@ def main():
                 lines_new.append(f"\t\t<published>{entry['time_formatted']}</published>") # Ex. 2021-07-28T20:57:31Z
                 lines_new.append(f"\t\t<updated>{entry['time_formatted']}</updated>")
                 lines_new.append(f"\t\t<link href=\"{url_html}\"/>")# Original RSS uses entry.links[0]['href']. .id is neater, and title doesn't need to be in link
-                lines_new.append(f"\t\t<author>")                   # Adding author of post (1/3)
-                lines_new.append(f"\t\t\t<name>@{username}</name>") # Adding author of post (2/3)
-                lines_new.append(f"\t\t</author>")                  # Adding author of post (3/3)
+                # Adding author of post
+                lines_new.append(f"\t\t<author>")
+                lines_new.append(f"\t\t\t<name>@{username}</name>")
+                lines_new.append(f"\t\t</author>")
+
                 lines_new.append(f"\t\t<category term=\"{RSS_TERM}\" label=\"{RSS_TITLE}\"/>")
                 lines_new.append(f"\t\t<content type=\"html\">")
                 lines_new.append(content_desc) # Adding HTML description to RSS
