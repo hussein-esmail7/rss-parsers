57b56a0deb607cb8f9e37893ea194d4ccea3dfd0
Hussein Esmail <hussein.esmail7@gmail.com>
2022-05-30 23:47:21 -0400

    Do not search for ducplicates

--- rss_vsco.py
+++ rss_vsco.py
@@ -87,16 +87,17 @@ def main():
         print(f"{str_prefix_info}Usernames found in urls file: {len(usernames)}")
 
     for username in usernames:
+        # For each username in the config file
+        # Resetting the variables that store URLs for each user
         site_base = f"https://vsco.co/{username}/"
         target_site = f"{site_base}gallery"
-        urls_images = []
-        dict_urls = []
-        lines_new = []
+        dict_urls = [] # Array of dictionaries that will store each found URL content
+        lines_new = [] # All new lines will be put here before going into the RSS file
         RSS_DESCRIPTION = f"VSCO pictures by @{username}" # For RSS feed posts
         RSS_TITLE = f"VSCO - @{username}"
+        entries_skipped = 0 # How many posts that are seen but the user already has
         if BOOL_PRINTS:
             print(f"{str_prefix_info}User: {username}")
-
         if not os.path.exists(RSS_FOLDER): # Make dir if it does not exist
             os.makedirs(RSS_FOLDER)
         if os.path.exists(RSS_FOLDER + f"{username}.xml"):
@@ -115,84 +116,110 @@ def main():
             # Replace template lines with filled in info
 
         driver.set_window_size(200, 1000)
-        driver.get(target_site)
+        driver.get(target_site) # Go to the user's VSCO homepage
         urls_all = [item.get_attribute("href") for item in driver.find_elements(By.XPATH, "//a[@href]")]
         if BOOL_PRINTS:
             print(f"{str_prefix_indent}\t This page has {len(urls_all)} posts")
         for item in urls_all:
             if f"{site_base}media/" in item:
-                urls_images.append(item)
                 dict_urls.append({
+                    "convert": True,
                     "url_html": item,
                     "url_imgs": [],
                     "time_formatted": "",
                     "desc": ""
                 })
+        len_orig = len(dict_urls)
+        # This reads what the RSS file is before making changes to it
+        # So that the program can read what URLs it has already
+        # So it doesn't waste processing time getting the image URLs of
+        # something that's in the file already and just skips duplicates
+        rss_file_searchable = open(RSS_FOLDER + f"{username}.xml", 'r').readlines()
+        # Make it all one line instead of an array
+        rss_file_searchable = "\n".join(rss_file_searchable).replace("\n", "")
+        for entry_num, entry in enumerate(dict_urls): # For each post URL
+            # This loop gets rid of entries that already exist in the RSS file,
+            # to save time of getting HTML links
+            if entry["url_html"] in rss_file_searchable and dict_urls[entry_num]["convert"]:
+                # print(f"{str_prefix_indent} Skipping {url_temp}")
+                dict_urls[entry_num]["convert"] = False # Do not convert this URL
+                entries_skipped += 1
+                # Entries are already sorted from newest to oldest
+                # Therefore, if it reaches one it already has, it has all
+                # future ones as well. Next part skips all future ones
+                for entry_num_i, entry_i in enumerate(dict_urls[entry_num+1:]):
+                    dict_urls[entry_num_i]["convert"] = False # Do not convert
+                    entries_skipped += 1
+        # Let the user know how many new posts out of all it got
+        print(str_prefix_indent + "Remaining: " + str(len_orig - entries_skipped) + " from " + str(len_orig))
+        # Visit each new URL
         for entry in dict_urls: # For each post URL
-            driver.get(entry["url_html"]) # Get HTML of each page
-            # Get URL for each image and add "https:" at start of each entry
-            for item in driver.find_elements(By.XPATH, "//img[@src]"):
-                url_unformatted = item.get_attribute("src")
-                if "im.vsco.co" in url_unformatted: # So that the favicon doesn't qualify as well (breaks the program)
-                    entry["url_imgs"] = ["https:" + url_unformatted[:url_unformatted.index("?")]]
-            # Get the time the post in question is posted
-            time_unformatted = driver.find_element(By.XPATH, '//time/span[1]').text + " /" + driver.find_element(By.XPATH, '//time/span[2]').text
-            # Convert letter casing to match RSS format
-            time_unformatted = time_unformatted.replace("pm", "PM").replace("am", "AM")
-            if len(time_unformatted.split("/")[1].split(":")[0]) == 1:
-                time_unformatted = time_unformatted.replace("/", "/0")
-            # Convert string to datetime object
-            entry["time_formatted"] = datetime.datetime.strptime(time_unformatted, "%B %d, %Y /%H:%M%p")
-            if "PM" in time_unformatted:
-                entry["time_formatted"] = entry["time_formatted"] + datetime.timedelta(hours=12)
-            entry["time_formatted"] = entry["time_formatted"].astimezone().replace(microsecond=0).isoformat()
-            # Get caption of the post
-            description_possible = driver.find_elements(By.XPATH, '//p')
-            for item in description_possible:
-                if "css-1whdjid-Caption" in item.get_attribute("class") and len(item.text) != 0:
-                    entry['desc'] = item.text
-            if not is_in_list(entry["url_html"], lines):
-                # If post is not in RSS, add it
-                # These variables are used for the RSS post
-                url_html = entry["url_html"] # URL of the post
-                content_desc = f"<p>{entry['desc']}</p>"
-                content_desc += f"\t\t<p><a href='{target_site}'>Profile</a></p>"
-                # Add each image URL
-                for img_url in entry["url_imgs"]:
-                    content_desc += f"\t\t<p><a href='{img_url}'>VSCO Image URL</a></p>"
-                # Converting HTML text to RSS HTML
-                content_desc = content_desc.replace("\n", "")
-                content_desc = content_desc.replace("\t", "")
-                # ":before" and ":after" (not needed)
-                content_desc = content_desc.replace("&#13;", "")
-                # Replace HTML elements in RSS description
-                content_desc = content_desc.replace("&", "&amp;")
-                content_desc = content_desc.replace("'", "&apos;")
-                content_desc = content_desc.replace('"', "&quot;")
-                content_desc = content_desc.replace("<", "&lt;")
-                content_desc = content_desc.replace(">", "&gt;")
-                lines_new.append(f"\t<entry>")
-                if len(entry['desc']) > 0:
-                    lines_new.append(f"\t\t<title>{entry['desc']}</title>")
-                else:
-                    lines_new.append(f"\t\t<title>New post by @{username}</title>")
-                lines_new.append(f"\t\t<published>{entry['time_formatted']}</published>") # Ex. 2021-07-28T20:57:31Z
-                lines_new.append(f"\t\t<updated>{entry['time_formatted']}</updated>")
-                lines_new.append(f"\t\t<link href=\"{url_html}\"/>")# Original RSS uses entry.links[0]['href']. .id is neater, and title doesn't need to be in link
-                # Adding author of post
-                lines_new.append(f"\t\t<author>")
-                lines_new.append(f"\t\t\t<name>@{username}</name>")
-                lines_new.append(f"\t\t</author>")
+            if entry["convert"]: # If the program needs to convert that URL
+                driver.get(entry["url_html"]) # Get HTML of each page
+                # Get URL for each image and add "https:" at start of each entry
+                for item in driver.find_elements(By.XPATH, "//img[@src]"):
+                    url_unformatted = item.get_attribute("src")
+                    if "im.vsco.co" in url_unformatted: # So that the favicon doesn't qualify as well (breaks the program)
+                        entry["url_imgs"] = ["https:" + url_unformatted[:url_unformatted.index("?")]]
+                # Get the time the post in question is posted
+                time_unformatted = driver.find_element(By.XPATH, '//time/span[1]').text + " /" + driver.find_element(By.XPATH, '//time/span[2]').text
+                # Convert letter casing to match RSS format
+                time_unformatted = time_unformatted.replace("pm", "PM").replace("am", "AM")
+                if len(time_unformatted.split("/")[1].split(":")[0]) == 1:
+                    time_unformatted = time_unformatted.replace("/", "/0")
+                # Convert string to datetime object
+                entry["time_formatted"] = datetime.datetime.strptime(time_unformatted, "%B %d, %Y /%H:%M%p")
+                if "PM" in time_unformatted:
+                    # Converting 12 hour to 24 hour
+                    entry["time_formatted"] = entry["time_formatted"] + datetime.timedelta(hours=12)
+                entry["time_formatted"] = entry["time_formatted"].astimezone().replace(microsecond=0).isoformat()
+                # Get caption of the post
+                description_possible = driver.find_elements(By.XPATH, '//p')
+                for item in description_possible:
+                    if "css-1whdjid-Caption" in item.get_attribute("class") and len(item.text) != 0:
+                        entry['desc'] = item.text
+                if not is_in_list(entry["url_html"], lines):
+                    # If post is not in RSS, add it
+                    # These variables are used for the RSS post
+                    url_html = entry["url_html"] # URL of the post
+                    content_desc = f"<p>{entry['desc']}</p>"
+                    content_desc += f"<p><a href='{target_site}'>Profile</a></p>"
+                    # Add each image URL
+                    for img_url in entry["url_imgs"]:
+                        content_desc += f"<p><a href='{img_url}'>VSCO Image URL</a></p>"
+                    # Converting HTML text to RSS HTML
+                    content_desc = content_desc.replace("\n", "")
+                    content_desc = content_desc.replace("\t", "")
+                    # ":before" and ":after" (not needed)
+                    content_desc = content_desc.replace("&#13;", "")
+                    # Replace HTML elements in RSS description
+                    content_desc = content_desc.replace("&", "&amp;")
+                    content_desc = content_desc.replace("'", "&apos;")
+                    content_desc = content_desc.replace('"', "&quot;")
+                    content_desc = content_desc.replace("<", "&lt;")
+                    content_desc = content_desc.replace(">", "&gt;")
+                    lines_new.append(f"<entry>")
+                    if len(entry['desc']) > 0:
+                        lines_new.append(f"<title>{entry['desc']}</title>")
+                    else:
+                        lines_new.append(f"<title>New post by @{username}</title>")
+                    lines_new.append(f"<published>{entry['time_formatted']}</published>") # Ex. 2021-07-28T20:57:31Z
+                    lines_new.append(f"<updated>{entry['time_formatted']}</updated>")
+                    lines_new.append(f"<link href=\"{url_html}\"/>") # Original RSS uses entry.links[0]['href']. .id is neater, and title doesn't need to be in link
+                    # Adding author of post
+                    lines_new.append(f"<author>")
+                    lines_new.append(f"<name>@{username}</name>")
+                    lines_new.append(f"</author>")
 
-                lines_new.append(f"\t\t<category term=\"{RSS_TERM}\" label=\"{RSS_TITLE}\"/>")
-                lines_new.append(f"\t\t<content type=\"html\">")
-                lines_new.append(content_desc) # Adding HTML description to RSS
-                lines_new.append(f"\t\t</content>")
-                lines_new.append(f"\t</entry>") # End of RSS post
+                    lines_new.append(f"<category term=\"{RSS_TERM}\" label=\"{RSS_TITLE}\"/>")
+                    lines_new.append(f"<content type=\"html\">")
+                    lines_new.append(content_desc) # Adding HTML description to RSS
+                    lines_new.append(f"</content>")
+                    lines_new.append(f"</entry>") # End of RSS post
 
         lines_new = [line + "\n" for line in lines_new] # Done for formatting
-        rss_delimiter_pos = [line.strip() for line in lines].index(RSS_POS_INSERT)
-        lines = lines[:rss_delimiter_pos+1] + lines_new + lines[rss_delimiter_pos+1:] # Join the array
+        rss_delimiter_pos = [line.strip() for line in lines].index(RSS_POS_INSERT) # Find the RSS Delimiter out of the stripped version of the array (each line stripped)
+        lines = lines[:rss_delimiter_pos+1] + lines_new + lines[rss_delimiter_pos+1:] # Join the array at the RSS delimiter position
         open(RSS_FOLDER + f"{username}.xml", 'w').writelines(lines) # Replace previous RSS lines
 
     # Cleanup
